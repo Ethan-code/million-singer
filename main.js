ce61eb388fb5c59305796464c801e6de0cc908ef
Ethan Ciou <ethan.ciou@aetherai.com>
2026-01-06 22:00:49 +0800

    change to JS version

--- main.js
+++ main.js
@@ -1,31 +1,31 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
 
 function createWindow() {
-    const win = new BrowserWindow({
-        width: 1280,
-        height: 720,
-        // 隱藏選單列 (讓它看起來更像遊戲)
-        autoHideMenuBar: true, 
-        webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false,
-            // 關鍵！關閉網頁安全檢查，允許讀取本機檔案
-            webSecurity: false 
-        }
-    });
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    // 隱藏選單列 (讓它看起來更像遊戲)
+    autoHideMenuBar: true,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      // 關鍵！關閉網頁安全檢查，允許讀取本機檔案
+      webSecurity: false,
+    },
+  });
 
-    win.loadFile('index.html');
+  win.loadFile("index.html");
 }
 
 app.whenReady().then(() => {
-    createWindow();
+  createWindow();
 
-    app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) createWindow();
-    });
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
 });
 
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit();
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
 });
